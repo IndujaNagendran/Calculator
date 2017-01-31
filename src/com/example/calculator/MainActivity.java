98b87ff40df8fcc9893da0c5e8fb0205d1c898ce
INDUJA N <indhu.hari22@gmail.com>
2017-01-31 14:32:33 +0530

    third commit

--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -4,14 +4,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
-
+	Button one,two,three,four,five,six,seven,eight,nine,zero;
+    EditText value;
+    Button enter;
+	Button clear;
+    Button sum;
+	Button diff;
+	Button multi;
+	Button div;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zero = (Button)findViewById(R.id.zero);
+        one = (Button)findViewById(R.id.one);
+        two = (Button)findViewById(R.id.two);
+        three = (Button)findViewById(R.id.three);
+        four = (Button)findViewById(R.id.four);
+        five = (Button)findViewById(R.id.five);
+        six = (Button)findViewById(R.id.six);
+        seven = (Button)findViewById(R.id.seven);
+        eight = (Button)findViewById(R.id.eight);
+        nine = (Button)findViewById(R.id.nine);
+        sum = (Button)findViewById(R.id.sum);
+        diff = (Button)findViewById(R.id.diff);
+        multi = (Button)findViewById(R.id.multi);
+        div = (Button)findViewById(R.id.div);
+        enter = (Button)findViewById(R.id.enter);
+        clear = (Button)findViewById(R.id.clear);
+        value = (EditText)findViewById(R.id.value);
+ 
+        
     }
 
     @Override
